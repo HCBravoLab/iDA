84a39447150d9bbbcc19568aaa52bd38a4d7ce07
reesyxan <46451408+reesyxan@users.noreply.github.com>
2021-04-26 13:26:30 -0400

    Update LDA.R

--- R/LDA.R
+++ R/LDA.R
@@ -192,13 +192,13 @@ decomposesvd <- function(withinclust_sc_mat,
     svd <- svd(solve(withinclust_sc_mat) %*% betweenclust_sc_mat, nu)
     top_eigenvectors <- svd$u[,1:nu]
     top_eigenvalues <- svd$d[1:nu]
-    return(list(top_eigenvectors, top_eigenvalues))
+    return(top_eigenvectors)
   } else if(is.numeric(set.seed)) {
     set.seed <- set.seed
     svd <- svd(solve(withinclust_sc_mat) %*% betweenclust_sc_mat, nu)
     top_eigenvectors <- svd$u[,1:nu]
     top_eigenvalues <- svd$d[1:nu]
-    return(list(eigenvecs = top_eigenvectors, eigenvalues = top_eigenvalues))
+    return(eigenvecs = top_eigenvectors)
   }
 }
 
