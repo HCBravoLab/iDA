12711ebe2cbc6389a2ec161f330515239e7d0ee2
reesyxan <46451408+reesyxan@users.noreply.github.com>
2021-05-25 10:23:44 -0400

    Update iDA.R

--- R/iDA.R
+++ R/iDA.R
@@ -19,7 +19,7 @@
 #' @param dims.use (numeric) A vector of the dimensions to use in construction of the SNN
 #' graph (e.g. To use the first 10 PCs, pass 1:10) (passed to [`getSNN`])
 #' @param diag Diagonalize the within class scatter matrix (assume the features are independent
-#' within each cluster)
+#'  within each cluster)
 #' @param set.seed (numeric or FALSE) seed random number generator before building KNN graph. (passed to [`getSNN`])
 #' @param c.param (numeric) Defines the number of desired clusters to be found in the embedding
 #' @param cluster.method What clustering method to use 
@@ -29,49 +29,51 @@
 #' @import plyr
 #' @return n number of dataframes for each cluster's data
 #'
-#'@export
 
+#' @export
 iDA_core <- function(data.use,
-                     NormCounts = NULL, 
-                     scaled = TRUE,
+                     NormCounts = NULL,
+                     scaled = FALSE,
                      var.Features = "scran",
-                     mean.low.cutoff = 0.1, 
+                     mean.low.cutoff = 0.1,
                      mean.high.cutoff = 8,
                      dispersion.cutoff = 1,
                      k.param = 10,
                      prune.SNN = 1/15,
                      dims.use = 10,
-                     diag = TRUE, 
-                     set.seed = FALSE, 
+                     diag = TRUE,
+                     set.seed = FALSE,
                      c.param = NULL,
-                     cluster.method = "walktrap"
-){
-  
-  # if (scaled == FALSE){
-  #normalize data by dividing by the sum of cell feature counts and then multiplying the cell counts by 10000
-  #  data.use.norm <- Matrix::t((Matrix::t(data.use)/ Matrix::colSums(data.use))* 10000)
-  # data.use.norm <- log1p(data.use.norm)
-  
-  #scale data with max 10
-  #data.use.scaled <- scale(data.use.norm)
-  #  }
-  
-  #find variable features
-  #  svd_time <- 0 
+                     cluster.method = "walktrap") {
+
+  ## if (scaled == FALSE){
+  ## normalize data by dividing by the sum of cell feature counts and then multiplying the cell counts by 10000
+  ## data.use.norm <- Matrix::t((Matrix::t(data.use)/ Matrix::colSums(data.use))* 10000)
+  ## data.use.norm <- log1p(data.use.norm)
+
+  ## scale data with max 10
+  ## data.use.scaled <- scale(data.use.norm)
+  ##  }
+
+  ## find variable features
+
   if (var.Features == "scran") {
     stats <- scran::modelGeneVar(NormCounts)
-    if (dim(data.use)[1] < 3000){
+    if (dim(data.use)[1] < 3000) {
       var.features <- rownames(data.use)
     } else {
       var.features <- scran::getTopHVGs(stats, n = 3000)
     }
-    
   } else if (var.Features == "disp") {
     if (is.null(NormCounts)) {
-      # variance stabilizing transformation using Deseq2
+      ## variance stabilizing transformation using Deseq2
       NormCounts <- varianceStabilizingTransformation(data.use)
     }
     var.features <- VariableGenes(NormCounts, dispersion.cutoff = dispersion.cutoff, mean.low.cutoff = mean.low.cutoff, mean.high.cutoff = mean.high.cutoff)
+  } else {
+    ## Use it all?
+    message("Potentially spuriously using all data.")
+    var.features <- rownames(data.use)
   }
   
   if(length(var.features) == 0){
@@ -81,7 +83,6 @@ iDA_core <- function(data.use,
   message(length(var.features), " variable features found. \n")
   
   #calculate svd for covariance matrix of variable_features
-  #start_svd <- Sys.time()
   var_data <- data.use[var.features,]
   if(!is.numeric(set.seed)){
     svd <- svdr(as.matrix(var_data), k = dims.use)
@@ -89,11 +90,12 @@ iDA_core <- function(data.use,
     set.seed(set.seed)
     svd <- svdr(as.matrix(var_data), k = dims.use)
   }
-  #end_svd <- Sys.time()
-  #svd_time <- svd_time + (end_svd - start_svd)
-  #transform data
+  # end_svd <- Sys.time()
+  # svd_time <- svd_time + (end_svd - start_svd)
+  # transform data
   transformed <- svd$v
   rownames(transformed) <- colnames(var_data)
+<<<<<<< HEAD
   
   #calculate SNN matrix for top PC's
   #louvain_time <- 0
@@ -150,19 +152,17 @@ iDA_core <- function(data.use,
     
     #merge data with cluster
     currentcluster <- as.data.frame(clusters[,i + 1])
-    #rownames(currentcluster) <- rownames(clusters)
+    # rownames(currentcluster) <- rownames(clusters)
     merged <- merge(currentcluster, t(var_data), by = 0)
-    
-    #split by cluster
+
+    # split by cluster
     splitclusters <- split_clusters(merged, merged[,2])
-    
-    #calculate within cluster scatter matrix
+
+    # calculate within cluster scatter matrix
     Sw <- withinclass_scattermatrix_LDA(splitclusters = splitclusters, diag = diag)
-    
-    #calculate between cluster scatter matrix
+
+    # calculate between cluster scatter matrix
     Sb <- betweenclass_scatter_matrix(splitclusters = splitclusters)
-    
-    #   start_svd = Sys.time()
     eigenvecs <- decomposesvd(Sw, Sb, nu = length(splitclusters) - 1, set.seed = set.seed)[["eigenvecs"]]
     
     #transform data
@@ -200,23 +200,24 @@ iDA_core <- function(data.use,
     }
     
     concordance <- mclust::adjustedRandIndex(clusters[,(dim(clusters)[2]-1)], clusters[,(dim(clusters)[2])])
-    
-    #end_louvain = Sys.time()
-    
-    #louvain_time = louvain_time + (end_louvain - start_louvain)
     i = i + 1
   }
-  
+
   geneweights <- as.data.frame(eigenvecs)
-  
+
   rownames(geneweights) <- var.features
   colnames(geneweights) <- paste("LD", 1:dim(geneweights)[2], sep = "")
-  
+
   rownames(eigenvectransformed) <- rownames(transformed)
   colnames(eigenvectransformed) <- paste("LD", 1:dim(eigenvectransformed)[2], sep = "")
-  
-  message(paste0("final concordance: "))
-  message(paste0(concordance))
-  return(list(clusters[,(dim(clusters)[2])], eigenvectransformed, geneweights, var.features))
-  
+
+  message("final concordance: ")
+  message(concordance)
+  ## What are the names for this list?
+  retlist <- list(clusters[, dim(clusters)[2]],
+                  eigenvectransformed,
+                  geneweights,
+                  var.features)
+
+  return(retlist)
 }
